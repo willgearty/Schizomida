148a3fc48004cda8939e954ae00dea243385f6ed
William Gearty <willgearty@gmail.com>
2023-09-25 15:53:46 -0400

    Remove Hmisc dependency

--- myapp/app.R
+++ myapp/app.R
@@ -18,8 +18,21 @@ library(htmltools)
 library(fontawesome)
 webr::install("prompter")
 library(prompter)
-webr::install("Hmisc")
-library(Hmisc)
+
+`%nin%` <- function(x, table) match(x, table, nomatch = 0) == 0
+
+all.is.numeric <- function(x, what = c("test", "vector", "nonnum"), extras = c(".", "NA")) {
+  what <- match.arg(what)
+  x <- sub("[[:space:]]+$", "", x)
+  x <- sub("^[[:space:]]+", "", x)
+  xs <- x[x %nin% c("", extras)]
+  if (!length(xs) || all(is.na(x))) 
+    return(switch(what, test = FALSE, vector = x, nonnum = x[0]))
+  isnon <- suppressWarnings(!is.na(xs) & is.na(as.numeric(xs)))
+  isnum <- !any(isnon)
+  switch(what, test = isnum, vector = if (isnum) suppressWarnings(as.numeric(x)) else x, 
+         nonnum = xs[isnon])
+}
 
 # load data ----
 shiny_schiz_orig <- read.csv("data/STDB.csv", check.names = FALSE)
