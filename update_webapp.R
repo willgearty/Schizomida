40ddfd80ca755facc057d1b3d8c481a6f4f60041
William Gearty <willgearty@gmail.com>
2024-06-27 11:57:07 -0400

    test github action

--- update_webapp.R
+++ update_webapp.R
@@ -4,8 +4,9 @@ library(shinylive)
 filename <- file.path(tempdir(), "temp_index.html")
 file.copy("docs/index.html", filename, overwrite = TRUE)
 
-# delete the "docs/shinylive" folder
+# delete shinylive assets file and folder
 # in case we are updating the shinylive assets version
+unlink("docs/shinylive-sw.js")
 unlink("docs/shinylive", recursive = TRUE)
 
 # update webapp
@@ -15,6 +16,8 @@ shinylive::export("myapp", "docs", verbose = FALSE, assets_version = "0.3.0")
 file.copy(filename, "docs/index.html", overwrite = TRUE)
 
 # run this to check that everything looks ok
-httpuv::runStaticServer("docs")
+if (Sys.getenv("CI") == "") {
+  httpuv::runStaticServer("docs")
+}
 
 # now commit and push!
