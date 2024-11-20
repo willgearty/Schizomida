18224a14bb6451505d71eca7daac9730b65c19fd
William Gearty <willgearty@gmail.com>
2024-11-20 16:18:05 -0500

    Add metadata

--- update_webapp.R
+++ update_webapp.R
@@ -1,5 +1,7 @@
 # use this file to generate the files for the webapp
 library(shinylive)
+library(metathis)
+library(dplyr)
 
 # delete shinylive assets file and folder
 # in case we are updating the shinylive assets version
@@ -14,7 +16,24 @@ template_params <- list(
                            '    <link rel="shortcut icon" href="/Schizomida/favicon.ico" />\n',
                            '    <link rel="apple-touch-icon" sizes="180x180" href="/Schizomida/apple-touch-icon.png" />\n',
                            '    <link rel="manifest" href="/Schizomida/site.webmanifest" />\n',
-                           '    <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>')
+                           '    <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>\n',
+                           '    ',
+                           paste0(meta() %>%
+                                    meta_description(
+                                      "The Schizomida Trait Data Base hosts taxonomic and morphological data on extant and extinct short-tailed whip-scorpions. The Data Base and website infrastructure are entirely open-source. The Data Base is hosted as a webapp using the shiny R framework and the webR/shinylive serverless infrastructure."
+                                    ) %>% 
+                                    meta_name("github-repo" = "willgearty/Schizomida") %>% 
+                                    meta_viewport() %>% 
+                                    meta_social(
+                                      title = "Schizomida Trait Data Base (STDB)",
+                                      url = "https://williamgearty.com/Schizomida/",
+                                      image = "https://williamgearty.com/Schizomida/schizomida.jpg",
+                                      image_alt = "A photo of Bucinozomus hortuspalmarum (c) Melvyn Yeo",
+                                      og_type = "website",
+                                      og_author = c("William Gearty", "Sandro Pascal Müller", "Ilian De Francesco Magnussen"),
+                                      twitter_card_type = "summary",
+                                      twitter_creator = "@willgearty"
+                                    ), collapse = '\n    '))
   )
 shinylive::export("myapp", "docs", assets_version = "0.5.0",
                   template_params = template_params)
